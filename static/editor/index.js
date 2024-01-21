e831b21acc4873fa6082313741e1aafa2eb6e448
aaaasmile <testhaus@gmx.at>
2024-01-21 20:09:42 +0100

    use unpkg only

--- static/editor/index.js
+++ static/editor/index.js
@@ -1,9 +1,9 @@
 import Yace from "https://unpkg.com/yace?module";
 
 import { highlight, defaultRenderers } from "https://unpkg.com/mdhl@0.0.6/dist/mdhl.esm.js?module";
-import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
-import go from "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/languages/go.min.js"
-hljs.registerLanguage('go', go);
+import hljs from 'https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/highlight.min.js';
+//import go from "https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/languages/go.min.js"
+//hljs.registerLanguage('go', go);
 
 const renderer = {
     ...defaultRenderers,
